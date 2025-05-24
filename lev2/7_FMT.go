b2979cfb38a775eeb6fccb23bf77956d415fb240
deepak kumar <deepakbhatiya7905@gmail.com>
2025-05-24 22:43:27 +0530

    formation fmt

---
+++ lev2/7_FMT.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/*
+it allow to print value
+Printing function :Print() Println() Printf()
+Formating functions :Sprint() Sprintf() Sprintln()
+scanning function :scan() Scanf() Scanln()
+error formating : Errorf()
+*/
+func main() {
+
+	fmt.Print("Helloe ", 123)
+
+}
